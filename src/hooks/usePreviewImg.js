63ec9f71195d7cf4569b8bc42e33a67ca81eef61
FrancisU10 <francisuko10@gmail.com>
2025-05-16 20:56:10 -0400

    Changed max photo size

--- src/hooks/usePreviewImg.js
+++ src/hooks/usePreviewImg.js
@@ -4,12 +4,12 @@ import useShowToast from "./useShowToast"
 const usePreviewImg = () => {
   const [selectedFile, setSelectedFile] = useState(null)
   const showToast = useShowToast()
-  const maxFileSizeInBytes = 2 * 1024 * 1024
+  const maxFileSizeInBytes = 8 * 1024 * 1024
   const handleImageChange = (e) => {
     const file = e.target.files[0]
     if (file && file.type.startsWith("image/")) {
         if (file.size > maxFileSizeInBytes) {
-            showToast("Error", "File size must be less than 2MB", "error")
+            showToast("Error", "File size must be less than 8MB", "error")
             setSelectedFile(null)
             return
         }
